83304fb882926fea9260ab1b49e22d30ef49c736
ponyii <ponyii@protonmail.com>
2023-08-30 20:43:44 +0400

    refactoring

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
 use std::{mem::MaybeUninit, sync::Arc, time::Duration, time::Instant};
 
-use pnet::packet::{Packet, icmp::echo_request::MutableEchoRequestPacket};
-use tokio::sync::oneshot;
+use pnet::packet::{icmp::echo_request::MutableEchoRequestPacket, Packet};
+use socket2::Socket;
+use tokio::{
+    sync::{oneshot, oneshot::Receiver, oneshot::Sender},
+    task::JoinHandle,
+};
 
 use utils::{
     create_icmp_request_packet, read_socket, socket, validate_icmp_response_packet, RESPONSE_BYTES,
@@ -22,51 +26,47 @@ const PING_INTERVAL_MS: u64 = 1;
 const IDENTIFIER: u16 = 100;
 const ECHO_TIMEOUT_SEC: u64 = 5;
 
-#[tokio::main]
-async fn main() {
-    let socket = socket(ADDRESS);
-    let socket_for_listener = Arc::new(socket);
-    let socket_for_sender = socket_for_listener.clone();
+enum Message {
+    Ok(u16, Duration),
+    Timeout(u16),
+    UnexpectedEcho(u16),
+}
 
-    let mut request_buf = [0; MutableEchoRequestPacket::minimum_packet_size()];
+impl Message {
+    fn publish(self) {
+        match self {
+            Self::Ok(seq, dur) => println!("{},{},{}", ADDRESS, seq, dur.as_micros()),
+            Self::Timeout(seq) => println!("Timeout ({})", seq),
+            Self::UnexpectedEcho(seq) => println!("Unexpected echo reply ({})", seq),
+        }
+    }
+}
 
-    // `senders[i]` and `receiver[i]` pertain to the `i`th sent request.
-    let mut receivers = Vec::with_capacity(PING_COUNT as usize);
-    let mut senders = Vec::with_capacity(PING_COUNT as usize);
-    for _ in 0..PING_COUNT {
+fn create_oneshots(num: usize) -> (Vec<Sender<Sender<Time>>>, Vec<Receiver<Sender<Time>>>) {
+    let mut receivers = Vec::with_capacity(num);
+    let mut senders = Vec::with_capacity(num);
+    for _ in 0..num {
         let (tx, rx) = oneshot::channel::<oneshot::Sender<Time>>();
         receivers.push(rx);
         senders.push(tx);
     }
+    (senders, receivers)
+}
 
-    let mut handles = Vec::with_capacity(PING_COUNT as usize);
-
-    // Spawn a listener
-    tokio::spawn(async move {
-        let mut buf: [MaybeUninit<u8>; RESPONSE_BYTES] = [MaybeUninit::uninit(); RESPONSE_BYTES];
-        loop {
-            let len = read_socket(&socket_for_listener, &mut buf).await;
-            let received_at = Instant::now();
-            let seq = validate_icmp_response_packet(&buf, len).unwrap();
-            match receivers[seq as usize].try_recv() {
-                Ok(tx) => {
-                    if let Err(_) = tx.send(received_at) {
-                        eprintln!("Receiver dropped ({})", seq);
-                    }
-                }
-                // It doesn't seem usefull to separate the cases of
-                // too early and duplicated responses.
-                Err(_) => eprintln!("Unexpected echo reply ({})", seq),
-            };
-        }
-    });
-
-    // Send requests and spawn response awaiting tasks for each
+// Send requests and spawn response awaiting tasks for each
+async fn send_requsts(
+    num: u16,
+    socket: Arc<Socket>,
+    mut senders: Vec<Sender<Sender<Time>>>,
+) -> Vec<JoinHandle<()>> {
+    let mut request_buf = [0; MutableEchoRequestPacket::minimum_packet_size()];
+    let mut handles = Vec::with_capacity(num as usize);
     senders.reverse(); // Get ready to element `pop`ping
+
     let mut interval = tokio::time::interval(Duration::from_millis(PING_INTERVAL_MS));
-    for i in 0..PING_COUNT {
+    for i in 0..num {
         let p = create_icmp_request_packet(&mut request_buf, i, IDENTIFIER);
-        socket_for_sender.send(p.packet()).unwrap();
+        socket.send(p.packet()).unwrap();
 
         let sent_at = Instant::now();
         let (tx, rx) = oneshot::channel::<Time>();
@@ -75,16 +75,50 @@ async fn main() {
             let res = tokio::time::timeout(Duration::from_secs(ECHO_TIMEOUT_SEC), rx).await;
             match res {
                 Ok(Ok(received_at)) => {
-                    println!("{},{},{}", ADDRESS, i, (received_at - sent_at).as_micros())
+                    let dur = received_at - sent_at;
+                    Message::Ok(i, dur).publish();
                 }
                 Ok(Err(e)) => eprintln!("Sender dropped ({}): {}", i, e),
-                Err(_) => println!("Timeout ({})", i),
+                Err(_) => Message::Timeout(i).publish(),
             }
         }));
 
         interval.tick().await;
     }
+    handles
+}
 
+#[tokio::main]
+async fn main() {
+    let socket = socket(ADDRESS);
+    let socket_for_listener = Arc::new(socket);
+    let socket_for_sender = socket_for_listener.clone();
+
+    // `senders[i]` and `receiver[i]` pertain to the `i`th sent request.
+    let (senders, mut receivers) = create_oneshots(PING_COUNT as usize);
+
+    // Spawn a listener
+    tokio::spawn(async move {
+        let mut buf: [MaybeUninit<u8>; RESPONSE_BYTES] = [MaybeUninit::uninit(); RESPONSE_BYTES];
+        loop {
+            let len = read_socket(&socket_for_listener, &mut buf).await;
+            let received_at = Instant::now();
+            let seq = validate_icmp_response_packet(&buf, len).unwrap();
+            // Inform the dedicated task about the received response.
+            match receivers[seq as usize].try_recv() {
+                Ok(tx) => {
+                    if let Err(_) = tx.send(received_at) {
+                        eprintln!("Receiver dropped ({})", seq);
+                    }
+                }
+                // It doesn't seem usefull to separate the cases of
+                // too early and duplicated responses.
+                Err(_) => Message::UnexpectedEcho(seq).publish(),
+            };
+        }
+    });
+
+    let handles = send_requsts(PING_COUNT, socket_for_sender, senders).await;
     for handle in handles {
         handle.await.expect("Task paniced");
     }
