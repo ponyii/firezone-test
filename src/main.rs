801b7bae93009cd37af12fd83916945c3b31b623
ponyii <ponyii@protonmail.com>
2023-08-30 17:01:48 +0400

    timeout implemented

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use std::net::SocketAddrV4;
-use std::{mem::MaybeUninit, sync::Arc, time::Duration};
+use std::{io::ErrorKind, mem::MaybeUninit, sync::Arc, time::Duration};
 
 use pnet::packet::{
     icmp::{
@@ -14,6 +14,7 @@ use socket2::{SockAddr, Socket};
 const ADDRESS: &str = "8.8.8.8";
 // Identifier randomization would be necessary to use multiple ICMP servers at once.
 const IDENTIFIER: u16 = 100;
+const ECHO_TIMEOUT_SEC: u64 = 5;
 const RESPONSE_BYTES: usize =
     EchoReplyPacket::minimum_packet_size() + Ipv4Packet::minimum_packet_size();
 
@@ -21,8 +22,8 @@ fn to_sock_addr(a: &str) -> SockAddr {
     SocketAddrV4::new(a.parse().unwrap(), 0).into()
 }
 
-// #[tokio::main]
-fn main() {
+#[tokio::main]
+async fn main() {
     let socket = Socket::new(
         socket2::Domain::IPV4,
         socket2::Type::RAW,
@@ -30,6 +31,7 @@ fn main() {
     )
     .unwrap();
     socket.connect(&to_sock_addr(ADDRESS)).unwrap();
+    socket.set_nonblocking(true).unwrap();
 
     let mut request_buf = [0; MutableEchoRequestPacket::minimum_packet_size()];
     let mut response_buf: [u8; RESPONSE_BYTES] = [0; RESPONSE_BYTES];
@@ -38,13 +40,17 @@ fn main() {
 
     let p = create_icmp_request_packet(&mut request_buf, 0, IDENTIFIER);
     socket.send(p.packet()).unwrap();
-    loop {
-        let len = socket.recv(buf_ref).unwrap();
-        if validate_icmp_response_packet(&response_buf, len).unwrap() == 0 {
-            println!("response received");
-            break;
+    tokio::time::timeout(Duration::from_secs(ECHO_TIMEOUT_SEC), async {
+        loop {
+            let len = read_socket(&socket, buf_ref).await;
+            if validate_icmp_response_packet(&response_buf, len).unwrap() == 0 {
+                println!("response received");
+                break;
+            }
         }
-    }
+    })
+    .await
+    .unwrap();
 }
 
 fn create_icmp_request_packet(
@@ -71,4 +77,19 @@ fn validate_icmp_response_packet(buf: &[u8], len: usize) -> Option<u16> {
     let packet = Ipv4Packet::new(&buf[0..len])?;
     let echo_reply = EchoReplyPacket::new(packet.payload())?;
     Some(echo_reply.get_sequence_number())
+}
+
+async fn read_socket(socket: &Socket, buf: &mut [MaybeUninit<u8>]) -> usize {
+    loop {
+        match socket.recv(buf) {
+            Err(e) => {
+                if e.kind() == ErrorKind::WouldBlock {
+                    tokio::time::sleep(Duration::from_millis(1)).await;
+                } else {
+                    panic!("Something went wrong while reading the socket");
+                }
+            }
+            Ok(res) => return res,
+        }
+    }
 }
